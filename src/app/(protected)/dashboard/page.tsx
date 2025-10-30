b28cce801024966c0d01e2af6a609380720aab6c
Matthijs De Haeck <matthijs.dehaeck@vtk.be>
2025-10-30 14:09:44 +0100

    fixed manage events

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -96,6 +96,8 @@ export function MyEventsSection() {
   const companyEvents = React.useMemo(() => {
   const companyOptions = company?.options ?? [];
 
+  console.log(companyOptions)
+
   // Extract event IDs from the career_event_option_id objects
   const companyEventIds = companyOptions
       .map((opt: any) => opt.event) // 'event' is the string ID
